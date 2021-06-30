7f02e2bf751f7687e2f6ffa1ab058e123b5a309a
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-06-30 13:31:31 -0500

    006 Obteniendo los Artículos de la Base de Datos

--- blog_practica.sql
+++ blog_practica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-06-2021 a las 16:46:33
+-- Tiempo de generación: 30-06-2021 a las 20:26:30
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 8.0.5
 
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `articulo`
+-- Estructura de tabla para la tabla `articulos`
 --
 
-CREATE TABLE `articulo` (
+CREATE TABLE `articulos` (
   `id` int(11) NOT NULL,
   `titulo` varchar(200) NOT NULL,
   `extracto` varchar(200) NOT NULL,
@@ -37,10 +37,10 @@ CREATE TABLE `articulo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `articulo`
+-- Volcado de datos para la tabla `articulos`
 --
 
-INSERT INTO `articulo` (`id`, `titulo`, `extracto`, `fecha`, `texto`, `thumb`) VALUES
+INSERT INTO `articulos` (`id`, `titulo`, `extracto`, `fecha`, `texto`, `thumb`) VALUES
 (1, 'Titulo del Primer Post', 'Este es el primer blog de prueba para comprobar cómo es que se crea u blog desde cero utilizando PHP y MYSQL desde cero', '2021-06-30 14:22:25', 'Este es el contenido del blog del cual estamos utilizando una base de datos para la materia de aplicaciones web. El grado el cual estoy cursando es el 3 cuatrimestre y la carrera que decidí estudiar es la de Desarrollo de software multiplataforma\r\nEste blog nos servirá de ayuda para poder comprender mejor como es que podemos hacer un blog desde cero con la ayuda de PHP y MSQL, además las herramientas que estamos utilizando para crear este blog es Visual Studio y XAMPP para PHPMyAdmin y esto nos será de gran utilidad para poder hacer esta practica\r\nEspero que el blog que haga les guste mucho ya que es un gran proyecto es que realizaremos y nos dará mucho aprendizaje para que en un futuro como desarrolladores nos sea de grana utilidad estos aprendizajes que estamos adquiriendo. Mi nombre es Daniel Fernandez Guarneros del 3A de Desarrollo de Software Multiplataforma\r\n', '1.png'),
 (2, 'Titulo del Segundo Post', 'Este es el segundo blog de prueba para comprobar cómo es que se crea u blog desde cero utilizando PHP y MYSQL desde cero', '2021-06-30 14:22:25', 'Este es el contenido del blog del cual estamos utilizando una base de datos para la materia de aplicaciones web. El grado el cual estoy cursando es el 3 cuatrimestre y la carrera que decidí estudiar es la de Desarrollo de software multiplataforma\r\nEste blog nos servirá de ayuda para poder comprender mejor como es que podemos hacer un blog desde cero con la ayuda de PHP y MSQL, además las herramientas que estamos utilizando para crear este blog es Visual Studio y XAMPP para PHPMyAdmin y esto nos será de gran utilidad para poder hacer esta practica\r\nEspero que el blog que haga les guste mucho ya que es un gran proyecto es que realizaremos y nos dará mucho aprendizaje para que en un futuro como desarrolladores nos sea de grana utilidad estos aprendizajes que estamos adquiriendo. Mi nombre es Daniel Fernandez Guarneros del 3A de Desarrollo de Software Multiplataforma\r\n', '2.png'),
 (3, 'Titulo del Tercer Post', 'Este es el tercer blog de prueba para comprobar cómo es que se crea u blog desde cero utilizando PHP y MYSQL desde cero', '2021-06-30 14:26:17', 'Este es el contenido del blog del cual estamos utilizando una base de datos para la materia de aplicaciones web. El grado el cual estoy cursando es el 3 cuatrimestre y la carrera que decidí estudiar es la de Desarrollo de software multiplataforma\r\nEste blog nos servirá de ayuda para poder comprender mejor como es que podemos hacer un blog desde cero con la ayuda de PHP y MSQL, además las herramientas que estamos utilizando para crear este blog es Visual Studio y XAMPP para PHPMyAdmin y esto nos será de gran utilidad para poder hacer esta practica\r\nEspero que el blog que haga les guste mucho ya que es un gran proyecto es que realizaremos y nos dará mucho aprendizaje para que en un futuro como desarrolladores nos sea de grana utilidad estos aprendizajes que estamos adquiriendo. Mi nombre es Daniel Fernandez Guarneros del 3A de Desarrollo de Software Multiplataforma\r\n', '3.png'),
@@ -51,9 +51,9 @@ INSERT INTO `articulo` (`id`, `titulo`, `extracto`, `fecha`, `texto`, `thumb`) V
 --
 
 --
--- Indices de la tabla `articulo`
+-- Indices de la tabla `articulos`
 --
-ALTER TABLE `articulo`
+ALTER TABLE `articulos`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -61,9 +61,9 @@ ALTER TABLE `articulo`
 --
 
 --
--- AUTO_INCREMENT de la tabla `articulo`
+-- AUTO_INCREMENT de la tabla `articulos`
 --
-ALTER TABLE `articulo`
+ALTER TABLE `articulos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
