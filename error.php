822d14732c0fe8a4ba35fa049c56b5e6b696f63d
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-06-30 15:16:57 -0500

    009 Creando la Página de Error

---
+++ error.php
@@ -0,0 +1,14 @@
+<?php require 'admin/config.php'; ?>
+<?php require 'views/header.php'; ?>
+
+<div class="contenedor">
+    <div class="post">
+        <article>
+            <h2 class="titulo">Error</h2>
+            <br>
+            <p class="extracto">Verifica que las conecciones sean correctas </p>
+        </article>
+    </div>
+</div>
+
+<?php require 'views/footer.php'; ?>
