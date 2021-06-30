89673aea68f26a8b876f3d88b2bb4adb658e79e7
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-06-30 11:34:32 -0500

    002 Creando la Página de Inicio

---
+++ admin/config.php
@@ -0,0 +1,21 @@
+<?php
+
+define('RUTA', 'http://localhost/Aplicaciones Web/Practica_Blog_PHP_MYSQL/');
+
+$bd_config = array(
+    'basedatos' => 'blog_practica',
+    'usuario' => 'root',
+    'pass' => ''
+);
+
+$blog_config = array(
+    'post_por_pagina'=> '2',
+    'carpeta_imagenes' => 'imagenes/'
+);
+
+$blog_admin = array(
+    'usuario' => 'Daniel',
+    'password' => '123'
+);
+
+?>
