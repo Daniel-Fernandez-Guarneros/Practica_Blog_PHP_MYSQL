4b2bfa6a99225313fc9eea54d2c9d380f1517802
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-06-30 12:06:25 -0500

    003 Acabando la Página de Inicio

---
+++ views/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p class="copyright">Copyright © 2020 - 2021 Daniel Fernandez</p>
+</footer>
+</body>
+</html>
--- views/index.view.php
+++ views/index.view.php
@@ -1,37 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, 
-    user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo RUTA; ?>css/estilos.css">
-    <title>Blog</title>
-</head>
-<body>
-<header>
-    <div class="contenedor">
-        <div class="logo izquierda">
-            <p><a href="a">Mi primer blog</a></p>
-        </div>
-        
-        <div class="derecha">
-            <form name="busqueda" class="buscar" action="<?php echo RUTA; ?>/buscar.php" method="get">
-                <input type="text" name="busqueda" placeholder="Buscar">
-                <button type="submit" class="icono fa fa-search"></button>
-            </form>
-            
-            <nav class="menu">
-                <ul>
-                    <li><a href="https://twitter.com/94Danierse"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="https://www.facebook.com/dan.fernandez.947/"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#">Contacto<i class="icono fa fa-envelope"></i></a></li>
-                </ul>
-            </nav>
-        </div>
+<?php require 'header.php' ?>
+
+<div class="contenedor">
+    <div class="post">
+        <article>
+            <h2 class="titulo">Titulo del Articulo</h2>
+            <p class="fecha">30 de Junio del 2021</p>
+            <div class="thumb">
+                <a href="#">
+                    <img src="<?php echo RUTA; ?>/imagenes/1.png" alt="">
+                </a>
+            </div>
+            <p class="extracto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, beatae!</p>
+            <a href="#" class="continuar">Continuar Leyendo</a>
+        </article>
     </div>
-</header>
-</body>
-</html>
+
+    <div class="post">
+        <article>
+            <h2 class="titulo">Titulo del Articulo</h2>
+            <p class="fecha">30 de Junio del 2021</p>
+            <div class="thumb">
+                <a href="#">
+                    <img src="<?php echo RUTA; ?>/imagenes/2.png" alt="">
+                </a>
+            </div>
+            <p class="extracto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, beatae!</p>
+            <a href="#" class="continuar">Continuar Leyendo</a>
+        </article>
+    </div>
+
+    <?php require 'paginacion.php'; ?>
+</div>
+
+<?php require 'footer.php'; ?>
